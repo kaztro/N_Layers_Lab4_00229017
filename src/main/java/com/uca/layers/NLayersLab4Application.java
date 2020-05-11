2e1fc085190a0a7c07a89857e440faedd3070185
kaztro <00229017@uca.edu.sv>
2020-05-10 21:18:23 -0600

    Init commit

---
+++ src/main/java/com/uca/layers/NLayersLab4Application.java
@@ -0,0 +1,13 @@
+package com.uca.layers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NLayersLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NLayersLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/layers/NLayersLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.layers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NLayersLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
